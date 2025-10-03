80e17b771855021eeb0ed6c5b92d5f4f153334be
Logan Falkenberg <loganf0101@gmail.com>
2025-10-02 22:42:45 -0500

    Updated screen UI

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Screen = () => {
   return (
-    <div>0</div>
+    <div style={{
+        height: '100%',
+        display: 'flex',
+        fontSize: '2.5rem',
+        paddingRight: '20px',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+      }}>
+      <div>
+        <h1>0</h1>
+      </div>
+    </div>
   )
 }
 
