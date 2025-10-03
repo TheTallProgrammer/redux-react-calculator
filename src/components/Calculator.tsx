eb2b659cc1475f6ecd2a44da6ea5efa9471d4299
Logan Falkenberg <loganf0101@gmail.com>
2025-10-02 22:14:32 -0500

    Updated styles for button grid and made the zero button

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -13,28 +13,30 @@ const Calculator = () => {
     <div className={styles.calculatorWrapper}>
       <div className={styles.contentWrapper}>
         <div className={styles.screenWrapper}>
-          <Screen/>
+          <Screen />
         </div>
         <div className={styles.operatorWrapper}>
           {operators.map((operator: string) => (
-            <OperatorButton key={keyId + 1} operatorSymbol={operator} buttonColor='#000000' width={100} height={70} marginTop='20px'/>
+            <OperatorButton key={keyId + 1} operatorSymbol={operator} buttonColor='#000000' width={100} height={70} marginTop='20px' />
           ))}
         </div>
         <div className={styles.middleContentWrapper}>
-          <div className={styles.numbersWrapper}>
-            {numbers.map((num: string) => (
-              <NumberButton buttonNumber={num}/>
-            ))}
+          <div className={styles.moreMiddleWrapper}>
+            <div className={styles.numbersWrapper}>
+              {numbers.map((num: string) => (
+                <NumberButton buttonNumber={num} />
+              ))}
+            </div>
+            <div className={styles.specialOpWrapper} >
+              <OperatorButton operatorSymbol='Clear' buttonColor='#000000' width={100} height={195} />
+              <OperatorButton operatorSymbol='=' buttonColor='#ff7300ff' width={100} height={195} />
+            </div>
           </div>
-          <div className={styles.specialOpWrapper} > 
-            <OperatorButton operatorSymbol='Clear' buttonColor='#000000' width={100} height={195}/>
-            <OperatorButton operatorSymbol='=' buttonColor='#ff7300ff' width={100} height={195}/> 
+          <div className={styles.lowerWrapper}>
+          <div className={styles.lowerButtonWrapper}>
+            <NumberButton buttonNumber='0' width={230} />
           </div>
         </div>
-        <div className={styles.lowerWrapper}>
-          <div className={styles.lowerButtonWrapper}> 
-            
-          </div>
         </div>
       </div>
     </div>
--- src/components/NumberButton.tsx
+++ src/components/NumberButton.tsx
@@ -3,11 +3,15 @@ import type { numberProps } from '../types'
 import styles from '../styles/NumberButton.module.css'
 
 const NumberButton = (props: numberProps) => {
+  const numberButtonStyles: React.CSSProperties = {
+    width: props.width ? props.width : 100,
+    height: props.height ? props.height : 70,
+  }
   return (
-    <div className={styles.buttonWrapper}>
+    <div style={numberButtonStyles}>
       <button className={styles.buttonStyles}>{props.buttonNumber}</button>
     </div>
   )
 }
 
-export default NumberButton
+export default NumberButton;
